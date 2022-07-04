df82d9489a3a61d7c1ce0bb11580453f9433ebe1
tanveer <mdtanviralamht731@gmail.com>
2022-07-04 11:01:18 +0600

    server init and  db connected

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PROT || 5000;
+// midlleware
+app.use(cors());
+app.use(express.json());
+// mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.dikh4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// DB API
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("product-db")
+      .collection("product-collection");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
+  } finally {
+    //something
+  }
+}
+run().catch(console.dir);
+// initial API
+app.get("/", (req, res) => {
+  res.send("Red Onion Server Running");
+});
+app.listen(port, () => {
+  console.log(port, "Is running");
+});
