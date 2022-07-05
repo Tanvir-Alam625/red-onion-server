d7680ed837d12977f5109a9a21da542f1276f76f
tanveer <mdtanviralamht731@gmail.com>
2022-07-06 02:35:09 +0600

    productDetails and order api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 // midlleware
@@ -18,13 +18,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    // collections
     const productCollection = client
       .db("product-db")
       .collection("product-collection");
+    const orderCollection = client
+      .db("product-db")
+      .collection("orders-collection");
+    // all api connection
     app.get("/products", async (req, res) => {
       const query = {};
       const products = await productCollection.find(query).toArray();
       res.send(products);
+    });
+    // get orders data
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+    // order post api
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    // orders get api
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
+    // issue token api
+    app.put("/token", async (req, res) => {
+      const user = req.body;
     });
   } finally {
     //something
